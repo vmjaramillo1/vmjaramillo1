c274aef9a9c0b7085c6fede3ade153d4ce726d2e
VICTOR MANUEL JARAMILLO ESPINOZA <vjaramilloespinoza770@gmail.com>
2023-03-16 04:02:38 -0500

    Fix deploy

--- vmjaramillo1/src/@global/GlobalLenguajeProgress/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default } from "./GlobalLenguajeProgress";
-export type { GlobalLenguajeProgressProps } from "./GlobalLenguajeProgress.interface";
--- vmjaramillo1/src/app/interfaces/jsonFromHtmlAttr.interface.tsx
+++
@@ -1,3 +0,0 @@
-export interface JSONFromHTMLAttr {
-  // style: string | null;
-}
